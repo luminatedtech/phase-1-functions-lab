f6e3329cfa169f1933d161d0626f61791d2ff9dc
avalmercafe02@gmail.com <allenkim@Allens-MacBook-Pro.local>
2022-05-20 15:37:36 -0400

    commit tree bebbb6a60c8aff81274d480dd2c44cedab6f8b03

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    return Math.abs(distance - 42) 
+}
+console.log(distanceFromHqInBlocks(50))
+
+function distanceFromHqInFeet(distance) {
+    return distanceFromHqInBlocks(distance)*264
+}
+
+
+function distanceTravelledInFeet(start,destination) {
+    return Math.abs(start-destination)*264
+
+}
+
+function calculatesFarePrice(start,destination) {
+    if (Math.abs(start-destination)*264 < 400 )
+        return 0
+    else if (Math.abs(start-destination)*264 > 400 && Math.abs(start-destination)*264 <2000)
+        return ((Math.abs(start-destination)*264 -400) * .02)
+    else if (Math.abs(start-destination)*264>2000 && Math.abs(start-destination)*264<2500)
+        return 25 
+    else if (Math.abs(start-destination)*264 > 2500)
+        return "cannot travel that far"
+}   
+console.log(calculatesFarePrice(60,42))
